abbd4d9cd3844a7da34878c46b6d36a4df7cf528
Guilherme dos Santos Ferreira <guii.ti@outlook.com>
2022-08-28 18:32:50 -0300

    Desafio 09 | Portfólio - em progresso

---
+++ desafio09/assets/js/script.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector('#menu');
+const menuBurguer = document.querySelector('#menu-burguer');
+const menuItems = document.querySelector('.menu-items');
+const closeMenu = document.querySelector('.close-menu');
+
+
+menuBurguer.addEventListener('click', () => {
+    menu.classList.toggle("active");
+    closeMenu.style.display = 'block';
+
+    menuItems.addEventListener('click', () => {
+        menu.classList.remove("active");
+        closeMenu.style.display = 'none';
+    });
+
+
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove("active");
+        closeMenu.style.display = 'none';
+    });
+
+});
+
+
+
+
+
+
+
+
+
